839001476f2ca46935116692d52245e5db4cd923
Yasmine <abdallahyesmine2@gmail.com>
2026-02-21 12:30:39 +0100

    auth guard

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,16 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, RouterState } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideAnimationsAsync(),
+    provideRouter(routes), // ✅ ajoute withHashLocation
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
   ]
--- frontend/src/app/guards/auth-guard.ts
+++ frontend/src/app/guards/auth-guard.ts
@@ -1,21 +1,31 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+
+  ) { }
 
   canActivate(): boolean {
-    const token = localStorage.getItem('access_token');
-
-    if (token) {
-      return true;
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('access_token');
+      if (token) {
+        return true;
+      }
+      this.router.navigate(['/login']);
+      return false;
     }
 
-    this.router.navigate(['/login']);
-    return false;
+    // ✅ Côté serveur (SSR) → on laisse passer, le navigateur vérifiera
+    return true;
   }
+
 }
+
